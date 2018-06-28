360274ee0d863a83d7637ab5237e8d569b707152
Tri Ngo <nmtri1987@gmail.com>
2018-06-28 15:39:34 +0700

    Create singleton

---
+++ DesignPatterns/DesignPatterns/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private int _numberOfInstance = 0;
+        private static Singleton _instance = new Singleton();
+
+        public static int MyVar = 10;
+
+        public Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        private Singleton()
+        {
+            Console.WriteLine("Private ctor");
+            _numberOfInstance++;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.MyVar);
+        }
+    }
+}
